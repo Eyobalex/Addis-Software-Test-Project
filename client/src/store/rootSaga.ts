c489370c150c4545092941c5c07580b159e749c8
Eyob alex <44606173+Eyobalex@users.noreply.github.com>
2024-02-09 22:38:40 +0300

    minor change

--- client/src/store/rootSaga.ts
+++
@@ -1,9 +0,0 @@
-import { all, fork } from 'redux-saga/effects';
-
-import photosWatcherSaga from '../photo/saga';
-
-export function* rootSaga(): any {
-    yield all([fork(photosWatcherSaga)]);
-}
-
-export default rootSaga;
