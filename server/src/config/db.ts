0793c0448bf2dbd79a6008203171a2d894b7bab4
Eyob alex <44606173+Eyobalex@users.noreply.github.com>
2024-03-15 13:04:52 +0300

    minor change

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -4,7 +4,6 @@ import mongoose from "mongoose";
 
 
  function  connect(){
-    console.log("🚀 ~ connect ~ process.env.MONGO_URL:", process.env.MONGO_URL)
     const db =  mongoose.connect(process.env.MONGO_URL);
     // console.log(`database connected successfully`);
     return db;
